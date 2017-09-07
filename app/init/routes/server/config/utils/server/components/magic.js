847e314b5a427004ba9e14987d2bbef6a92835cd
unknown <novialim@gmail.com>
2017-09-06 18:30:10 -0700

    final

--- app/init/routes/server/config/utils/server/components/magic.js
+++
@@ -1,3 +0,0 @@
-var strWindowFeatures = "location=yes,height=570,width=520,scrollbars=yes,status=yes";
-var URL = "http://novialim.herokuapp.com/=" + location.href;
-var win = window.open(URL, "_blank", strWindowFeatures);
